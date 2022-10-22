147e0ec7a551d6bbfce073d5438b62f1028bdece
Vladislav Polivko <82750662+botonojke@users.noreply.github.com>
2022-10-22 12:00:08 +0300

    Correction of remarks

--- ex2/main.py
+++ ex2/main.py
@@ -4,49 +4,116 @@ import numpy as np
 import os
 
 
-def get_point(image):
-    image_name = image.split('\\')[-1]
-    pic = cv2.imread(f'D:\Python\Repo\-TrainingDataSolutions\ex2\images\\{image_name}', -1)
-    pick = pic.copy()
-    img = np.zeros(pic.shape, dtype='uint8')
-    mask = np.zeros(pic.shape, dtype='uint8')
-    channel_count = pic.shape[2]
-    ignore_mask_color = (255,) * channel_count
+def get_skin_color(xml_file: str):
+    """Get skin color from xml file"""
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    for meta in root.findall('meta'):
+        for task in meta.findall('task'):
+            for labels in task.findall('labels'):
+                for label in labels.findall('label'):
+                    if label.find('name').text == 'Skin':
+                        h = label.find('color').text.lstrip('#')
+                        return tuple(int(h[i:i + 2], 16) for i in (0, 2, 4))
+
+
+def get_point(label: str, image_name: str):
+    """Gets the coordinates for the masks and returns a dictionary with all the faces of the polygon"""
     tree = ET.parse('masks.xml')
     root = tree.getroot()
-    color = (115, 51, 128)
-    thickness = 2
+    ignore = dict()
+    skin = dict()
+    key_for_ignore = 0
+    key_for_skin = 0
+    pts = []
     for i in root.findall('image'):
         if i.attrib['name'].split('/')[-1] == image_name:
             for j in i.findall('polygon'):
                 pts = []
                 if j.attrib['label'] == 'Ignore':
-                    color = (250, 50, 83)
                     for cord in j.attrib['points'].split(';'):
-                        cord = list(map(float, cord.split(',')))
-                        pts.append(cord)
-                    pts = np.array(pts, np.int32)
-                    cv2.fillPoly(pic, pts=[pts], color=(0, 0, 0))
-                    cv2.fillPoly(img, pts=[pts], color=(0, 0, 0))
-                    cv2.fillPoly(mask, pts=[pts], color=ignore_mask_color)
+                        pts.append(list(map(float, cord.split(','))))
+                    key_for_ignore += 1
+                    ignore[key_for_ignore] = pts
 
                 if j.attrib['label'] == 'Skin':
-                    color = (115, 51, 128)
                     for cord in j.attrib['points'].split(';'):
-                        cord = list(map(float, cord.split(',')))
-                        pts.append(cord)
-                    pts = np.array(pts, np.int32)
-                    cv2.fillPoly(pic, pts=[pts], color=color)
-                    cv2.fillPoly(img, pts=[pts], color=color)
-
-    pickk = cv2.bitwise_and(pick, mask)
-    new_img = cv2.add(pic, pickk)
-    cv2.imwrite(f'D:\Python\Repo\-TrainingDataSolutions\ex2\\black_image_with_mask\\{image_name}', img)
-    cv2.imwrite(f'D:\Python\Repo\-TrainingDataSolutions\ex2\orig_image_with_mask\\{image_name}', new_img)
+                        pts.append(list(map(float, cord.split(','))))
+                    key_for_skin += 1
+                    skin[key_for_skin] = pts
+    if label == 'Skin':
+        return skin
+    elif label == 'Ignore':
+        return ignore
 
 
-for root, dirs, files in os.walk('images'):
-    for file in files:
-        if file.endswith(".jpg"):
-            path_file = os.path.join(root, file)
-            get_point(path_file)
+def get_black_img(image: str, labels: list, xml: str):
+    """Gets a mask on a black background"""
+    image_name = image.split('\\')[-1]
+    pic = cv2.imread(image, -1)
+    img = np.zeros(pic.shape, dtype='uint8')
+    color = get_skin_color(xml)
+    for label in labels:
+        if label == 'Skin':
+            pts = get_point(label='Skin', image_name=image_name)
+            for i, j in pts.items():
+                pts = np.array(j, np.int32)
+                cv2.fillPoly(img, pts=[pts], color=color)
+        elif label == 'Ignore':
+            pts = get_point(label='Ignore', image_name=image_name)
+            for i, j in pts.items():
+                pts = np.array(j, np.int32)
+                cv2.fillPoly(img, pts=[pts], color=(0, 0, 0))
+
+    return cv2.imwrite(f'D:\Python\Repo\-TrainingDataSolutions\ex2\\black_image_with_mask\\{image_name}', img)
+
+
+def get_mask_for_ignore(image: str, labels: list):
+    """Gets the mask to ignore on the original background"""
+    image_name = image.split('\\')[-1]
+    pic = cv2.imread(image, -1)
+    mask = np.zeros(pic.shape, dtype='uint8')
+    channel_count = pic.shape[2]
+    ignore_mask_color = (255,) * channel_count
+    for label in labels:
+        if label == 'Ignore':
+            pts = get_point(label='Ignore', image_name=image_name)
+            for i, j in pts.items():
+                pts = np.array(j, np.int32)
+                cv2.fillPoly(mask, pts=[pts], color=ignore_mask_color)
+
+    return mask
+
+
+def get_orig_img_with_mask(image: str, labels: list, xml: str):
+    """Gets a mask on original background"""
+    image_name = image.split('\\')[-1]
+    pic = cv2.imread(image, -1)
+    mask = get_mask_for_ignore(image, labels=labels)
+    img = cv2.bitwise_and(pic.copy(), mask)
+    color = get_skin_color(xml)
+    for label in labels:
+        if label == 'Skin':
+            pts = get_point(label='Skin', image_name=image_name)
+            for i, j in pts.items():
+                pts = np.array(j, np.int32)
+                cv2.fillPoly(pic, pts=[pts], color=color)
+        elif label == 'Ignore':
+            pts = get_point(label='Ignore', image_name=image_name)
+            for i, j in pts.items():
+                pts = np.array(j, np.int32)
+                cv2.fillPoly(pic, pts=[pts], color=(0, 0, 0))
+    pic = cv2.add(pic, img)
+
+    return cv2.imwrite(f'D:\Python\Repo\-TrainingDataSolutions\ex2\orig_image_with_mask\\{image_name}', pic)
+
+
+if __name__ == '__main__':
+    """Searches for all jpg files in the specified area"""
+    for root, dirs, files in os.walk('D:\Python\Repo\-TrainingDataSolutions\ex2\images'):
+        for file in files:
+            if file.endswith(".jpg"):
+                path_file = os.path.join(root, file)
+                get_orig_img_with_mask(image=path_file, labels=['Skin', 'Ignore'], xml='masks.xml')
+                get_black_img(image=path_file, labels=['Skin', 'Ignore'], xml='masks.xml')
